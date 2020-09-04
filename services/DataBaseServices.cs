a2e1fb2828a8e7c00864360f1eb890c0ac97c7ba
Paul Dudnic <paul.dudnic@grenoble-inp.fr>
2020-09-04 12:06:14 +0200

    Services

---
+++ services/DataBaseServices.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PricingLibrary.FinancialProducts;
+
+namespace ProjetPricing.services
+{
+    class DataBaseServices
+    {
+        public static List<Share> getShares()
+        {
+            List<Share> allShares = new List<Share>();
+            using (DataBaseAccessDataContext asdc = new DataBaseAccessDataContext())
+            {
+                var allSharesQuery = (from share in asdc.ShareNames select share).Distinct();
+                foreach (var share in allSharesQuery)
+                {
+                    Share newShare = new Share(share.name, share.id);
+                    allShares.Add(newShare);
+                }
+            }
+            return allShares;
+        }
+
+        public static Dictionary<DateTime, decimal> getShareValues(string shareID)
+        {
+            var values = new Dictionary<DateTime, decimal>();
+            using (DataBaseAccessDataContext asdc = new DataBaseAccessDataContext())
+            {
+                var shareQuery = (from share in asdc.HistoricalShareValues where share.id == shareID orderby share.date select share);
+                foreach (var share in shareQuery)
+                {
+                    values.Add(share.date, share.value);
+                }
+            }
+            return values;
+        }
+    }
+}

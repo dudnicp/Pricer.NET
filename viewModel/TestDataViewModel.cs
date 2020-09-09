f84032499ac5a52f5c7cb01a7b732d1ecddf38d4
Paul Dudnic <paul.dudnic@grenoble-inp.org>
2020-09-09 10:26:34 +0100

    Exceptions

--- viewModel/TestDataViewModel.cs
+++ viewModel/TestDataViewModel.cs
@@ -4,6 +4,7 @@ using PricingApp.services.optionPricers;
 using PricingApp.view;
 using PricingLibrary.Computations;
 using PricingLibrary.FinancialProducts;
+using PricingLibrary.Utilities;
 using PricingLibrary.Utilities.MarketDataFeed;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 using static PricingApp.model.OptionData;
 
@@ -47,52 +49,88 @@ namespace PricingApp.viewModel
                 {
                     launchTest = new RelayCommand<IDataFeedProvider>(dataFeedProvider => 
                     {
-                        GraphWindow graph = new GraphWindow();
-
-                        List<TrackedResults> results = new List<TrackedResults>();
-
-                        if(testData.TestedOptionData.UnderlyingShares.Count == 1)
+                        try
                         {
-                            Share share = TestData.TestedOptionData.UnderlyingShares.First().Share;
-                            VanillaCall option = new VanillaCall(TestData.TestedOptionData.Name, 
-                                share, 
-                                TestData.TestedOptionData.Maturity,
-                                TestData.TestedOptionData.Strike) ;
-                            VanillaCallPricer pricer = new VanillaCallPricer(option);
-                            PortfolioManager manager = new PortfolioManager(pricer, dataFeedProvider);
-                            results = manager.computePortfolioEvolution(TestData.TestStart,
-                            TestData.TestEnd,
-                            TestData.RebalancingPeriod,
-                            TestData.EstimationPeriod);
-                        }
-                        else if(testData.TestedOptionData.UnderlyingShares.Count > 1)
-                        {
-                            Share[] shares = new Share[TestData.TestedOptionData.UnderlyingShares.Count];
-                            double[] weight = new double[TestData.TestedOptionData.UnderlyingShares.Count];
-                            int i = 0;
-                            foreach(ShareAndWeight share in TestData.TestedOptionData.UnderlyingShares)
+                            if(TestData == null)
                             {
-                                shares[i] = share.Share;
-                                weight[i] = share.Weight;
-                                i++;
+                                throw new Exception("Aucune option n'a été séléctionnée pour être testée");
+                            }
+                            if(DateTime.Compare(TestData.TestEnd, TestData.TestStart) <= 0)
+                            {
+                                throw new Exception("Date de fin de test avant le début du test");
+                            }
+                            if (DateTime.Compare(TestData.TestedOptionData.Maturity, TestData.TestEnd) < 0)
+                            {
+                                throw new Exception("Date de maturité de l'option avant la fin du test");
+                            }
+                            if(TestData.RebalancingPeriod <= 0)
+                            {
+                                throw new Exception("Période de rebalancement invalide");
+                            }
+                            if (TestData.EstimationPeriod <= 0)
+                            {
+                                throw new Exception("Période d'estimation invalide");
                             }
 
-                            BasketOption option = new BasketOption(TestData.TestedOptionData.Name,
-                                shares,
-                                weight,
-                                TestData.TestedOptionData.Maturity,
-                                TestData.TestedOptionData.Strike);
-                            BasketOptionPricer pricer = new BasketOptionPricer(option);
-                            PortfolioManager manager = new PortfolioManager(pricer, dataFeedProvider);
-                            results = manager.computePortfolioEvolution(TestData.TestStart,
-                            TestData.TestEnd,
-                            TestData.RebalancingPeriod,
-                            TestData.EstimationPeriod);
+                            if (DayCount.CountBusinessDays(TestData.TestStart, TestData.TestEnd) <= TestData.RebalancingPeriod)
+                            {
+                                throw new Exception("La période de rebalancement dépasse le nombre de jours ouvrés de la période de test");
+                            }
+                            if(TestData.RebalancingPeriod < TestData.EstimationPeriod)
+                            {
+                                throw new Exception("La période d'estimation est plus grande que la période de rebalancement");
+                            }
+
+
+                            GraphWindow graph = new GraphWindow();
+
+                            List<TrackedResults> results = new List<TrackedResults>();
+                            if (testData.TestedOptionData.UnderlyingShares.Count == 1)
+                            {
+                                Share share = TestData.TestedOptionData.UnderlyingShares.First().Share;
+                                VanillaCall option = new VanillaCall(TestData.TestedOptionData.Name,
+                                    share,
+                                    TestData.TestedOptionData.Maturity,
+                                    TestData.TestedOptionData.Strike);
+                                VanillaCallPricer pricer = new VanillaCallPricer(option);
+                                PortfolioManager manager = new PortfolioManager(pricer, dataFeedProvider);
+                                results = manager.computePortfolioEvolution(TestData.TestStart,
+                                TestData.TestEnd,
+                                TestData.RebalancingPeriod,
+                                TestData.EstimationPeriod);
+                            }
+                            else if (testData.TestedOptionData.UnderlyingShares.Count > 1)
+                            {
+                                Share[] shares = new Share[TestData.TestedOptionData.UnderlyingShares.Count];
+                                double[] weight = new double[TestData.TestedOptionData.UnderlyingShares.Count];
+                                int i = 0;
+                                foreach (ShareAndWeight share in TestData.TestedOptionData.UnderlyingShares)
+                                {
+                                    shares[i] = share.Share;
+                                    weight[i] = share.Weight;
+                                    i++;
+                                }
+
+                                BasketOption option = new BasketOption(TestData.TestedOptionData.Name,
+                                    shares,
+                                    weight,
+                                    TestData.TestedOptionData.Maturity,
+                                    TestData.TestedOptionData.Strike);
+                                BasketOptionPricer pricer = new BasketOptionPricer(option);
+                                PortfolioManager manager = new PortfolioManager(pricer, dataFeedProvider);
+                                results = manager.computePortfolioEvolution(TestData.TestStart,
+                                TestData.TestEnd,
+                                TestData.RebalancingPeriod,
+                                TestData.EstimationPeriod);
+                            }
+
+                            graph.DataContext = new GraphViewModel(results);
+                            graph.Show();
                         }
-
-                        graph.DataContext = new GraphViewModel(results);
-                        graph.Show();
-
+                        catch (Exception e)
+                        {
+                            MessageBox.Show(e.Message, "Erreur", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
                     });
                 }
                 return launchTest;

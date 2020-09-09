7a9ab8ac378709ffe28a510dedad15e50eef4e1b
Paul Dudnic <paul.dudnic@grenoble-inp.org>
2020-09-09 07:23:40 +0100

    Graphs in separated Window

--- TestDataViewModel.cs
+++ TestDataViewModel.cs
@@ -22,212 +22,22 @@ using PricingApp.services.optionPricers;
 
 namespace PricingApp
 {
-    public class TestDataViewModel : INotifyPropertyChanged
+    public class GraphViewModel : INotifyPropertyChanged
     {
-        private string optionName;
-        private double optionStrike;
-        private DateTime optionMaturity;
-        private DateTime testStart;
-        private DateTime testEnd;
-        private IDataFeedProvider dataProvider;
-        private int rebalancingPeriod;
-        private int estimationPeriod;
-        private ObservableCollection<Share> optionUnderlyingShares;
         private SeriesCollection seriesCollection;
         private SeriesCollection seriesCollection2;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public void NotifyPropertyChanged([CallerMemberName] string str="")
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(str));
-        }
-
         public SeriesCollection SeriesCollection { get { return seriesCollection; } }
         public SeriesCollection SeriesCollection2 { get { return seriesCollection2; } }
-
-
-
-        public string OptionName
+        public GraphViewModel(List<TrackedResults> resultsEvolution)
         {
-            get
-            {
-                return optionName;
-            }
-            set
-            {
-                if(value != optionName)
-                {
-                    optionName = value;
-                    NotifyPropertyChanged();
-                }
-            }
-        }
-
-        public double OptionStrike
-        {
-            get
-            {
-                return optionStrike;
-            }
-            set
-            {
-                if (value != optionStrike)
-                {
-                    optionStrike = value;
-                    NotifyPropertyChanged();
-                }
-            }
-        }
-
-        public DateTime OptionMaturity
-        {
-            get
-            {
-                return optionMaturity;
-            }
-            set
-            {
-                if (value != optionMaturity)
-                {
-                    optionMaturity = value;
-                    NotifyPropertyChanged();
-                }
-            }
-        }
-
-        public DateTime TestStart
-        {
-            get
-            {
-                return testStart;
-            }
-            set
-            {
-                if (value != testStart)
-                {
-                    testStart = value;
-                    NotifyPropertyChanged();
-                }
-            }
-        }
-
-        public DateTime TestEnd
-        {
-            get
-            {
-                return testEnd;
-            }
-            set
-            {
-                if (value != testEnd)
-                {
-                    testEnd = value;
-                    NotifyPropertyChanged();
-                }
-            }
-        }
-
-        public IDataFeedProvider DataProvider
-        {
-            get
-            {
-                return dataProvider;
-            }
-            set
-            {
-                if (value != dataProvider)
-                {
-                    dataProvider = value;
-                    NotifyPropertyChanged();
-                }
-            }
-        }
-
-        public int RebalancingPeriod
-        {
-            get
-            {
-                return rebalancingPeriod;
-            }
-            set
-            {
-                if (value != rebalancingPeriod)
-                {
-                    rebalancingPeriod = value;
-                    NotifyPropertyChanged();
-                }
-            }
-        }
-        public int EstimationPeriod
-        {
-            get
-            {
-                return estimationPeriod;
-            }
-            set
-            {
-                if (value != estimationPeriod)
-                {
-                    estimationPeriod = value;
-                    NotifyPropertyChanged();
-                }
-            }
-        }
-
-        public ObservableCollection<Share> OptionUnderlyingShares
-        {
-            get
-            {
-                return optionUnderlyingShares;
-            }
-            set
-            {
-                if(value != optionUnderlyingShares)
-                {
-                    optionUnderlyingShares = value;
-                    NotifyPropertyChanged("OptionUnderlyingShares");
-                }
-            }
-        }
-
-        private ICommand addUnderlyingShare;
-        public ICommand AddUnderlyingShare
-        {
-            get
-            {
-                if (addUnderlyingShare == null)
-                {
-                    addUnderlyingShare = new RelayCommand<object>((obj) => OptionUnderlyingShares.Add(new Share("name", "id")));
-                }
-                return addUnderlyingShare;
-            }
-        }
-
-        private ICommand removeUnderlyingShare;
-        public ICommand RemoveUnderlyingShare
-        {
-            get
-            {
-                if (removeUnderlyingShare == null)
-                {
-                    removeUnderlyingShare = new RelayCommand<Share>((share) => OptionUnderlyingShares.Remove(share));
-                }
-                return removeUnderlyingShare;
-            }
-        }
-
-
-
-        public TestDataViewModel()
-        {
-            optionUnderlyingShares = new ObservableCollection<Share>();
-
             var dayConfig = Mappers.Xy<DataChartViewModel>()
                 .X(dayModel => (double)dayModel.Date.Ticks / TimeSpan.FromDays(1).Ticks)
                 .Y(dayModel => dayModel.Value);
 
-            Share share1 = new Share("ACCOR SA", "AC FP");
+            /*Share share1 = new Share("ACCOR SA", "AC FP");
             Share share2 = new Share("ALSTOM", "ALO FP");
             Share[] underlyingAsset = new Share[]{share1, share2 };
             VanillaCall opt0 = new VanillaCall("option", share1, new DateTime(2180, 2, 2), 5);
@@ -238,43 +48,43 @@ namespace PricingApp
 
             PortfolioManager manager = new PortfolioManager(pricer, new SimulatedDataFeedProvider());
 
-            // portfolio value 
-            ChartValues <DateTimePoint> chartValues = new ChartValues<DateTimePoint>();
-            List<TrackedResults> trackedList = manager.computePortfolioEvolution(new DateTime(2020, 1, 1), new DateTime(2025, 12, 1), 100, 20);
-            // option value
-            ChartValues<DateTimePoint> chartValues2 = new ChartValues<DateTimePoint>();
-            // tracking error
-            ChartValues<DateTimePoint> chartValues3 = new ChartValues<DateTimePoint>();
-            ChartValues<DateTimePoint> chartValues4 = new ChartValues<DateTimePoint>();
+            List<TrackedResults> trackedList = manager.computePortfolioEvolution(new DateTime(2020, 1, 1), new DateTime(2025, 12, 1), 100, 20);*/
 
 
-            foreach(TrackedResults res in trackedList)
+
+
+
+            ChartValues <DateTimePoint> portfolioValues = new ChartValues<DateTimePoint>();
+            ChartValues<DateTimePoint> payoffValues = new ChartValues<DateTimePoint>();
+            ChartValues<DateTimePoint> trackingErrorValues = new ChartValues<DateTimePoint>();
+            ChartValues<DateTimePoint> optionPriceValues = new ChartValues<DateTimePoint>();
+
+            foreach(TrackedResults res in resultsEvolution)
             {
-                chartValues.Add(new DateTimePoint(res.Date, res.PortfolioValue));
-                chartValues2.Add(new DateTimePoint(res.Date, res.Payoff));
-                chartValues3.Add(new DateTimePoint(res.Date, res.TrackingError));
-                chartValues4.Add(new DateTimePoint(res.Date, (res.PortfolioValue - res.Payoff) / res.TrackingError));
+                portfolioValues.Add(new DateTimePoint(res.Date, res.PortfolioValue));
+                payoffValues.Add(new DateTimePoint(res.Date, res.Payoff));
+                trackingErrorValues.Add(new DateTimePoint(res.Date, res.TrackingError));
+                optionPriceValues.Add(new DateTimePoint(res.Date, (res.PortfolioValue - res.Payoff) / res.TrackingError));
             }
 
-            
             seriesCollection = new SeriesCollection(dayConfig)
             {
-                new LineSeries // portfolio value
+                new LineSeries
                 {
                     Title = "Portfolio",
-                    Values = chartValues,
+                    Values = portfolioValues,
                     PointGeometrySize = 5
                 },
-                new LineSeries // payoff value
+                new LineSeries
                 {
                     Title = "Payoff",
-                    Values = chartValues2,
+                    Values = payoffValues,
                     PointGeometrySize = 5
                 },
-                new LineSeries // optionPrice
+                new LineSeries
                 {
                     Title = "Option",
-                    Values = chartValues4,
+                    Values = optionPriceValues,
                     PointGeometrySize = 5
                 },
 
@@ -283,10 +93,10 @@ namespace PricingApp
             seriesCollection2 = new SeriesCollection(dayConfig)
             {
 
-                new LineSeries // tracking error value
+                new LineSeries
                 {
                     Title = "Tracking Error",
-                    Values = chartValues3,
+                    Values = trackingErrorValues,
                     PointGeometrySize = 5
                 },
             };
